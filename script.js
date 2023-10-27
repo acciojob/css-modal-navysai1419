78d35ca7b665d94b1f4c953c4c03f2717cdbb70a
Navya <88023981+navysai1419@users.noreply.github.com>
2023-10-27 15:49:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+document.addEventListener('DOMContentLoaded', function () {
+    const openModalBtn = document.getElementById('openModal');
+    const modal = document.getElementById('myModal');
+    const closeModalBtn = document.querySelector('.close-modal');
+
+    openModalBtn.addEventListener('click', function () {
+        modal.style.display = 'block';
+    });
+
+    closeModalBtn.addEventListener('click', function () {
+        modal.style.display = 'none';
+    });
+
+    window.addEventListener('click', function (event) {
+        if (event.target === modal) {
+            modal.style.display = 'none';
+        }
+    });
+});
